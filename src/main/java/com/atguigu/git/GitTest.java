1d41f17271b511cf4ef9b0d26284a3174d585c0e
Hruyi <2917398357@qq.com>
2023-02-11 12:24:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author HuangRuYi
+ * @date 2023/2/11 - 12:17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
